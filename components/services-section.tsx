099ac3e9a2d5acad5a4f4e7eba44f6753b408257
Toma Alexandru <alextoma1704@gmail.com>
2025-05-17 21:19:20 +0300

    Fixed build errors

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -145,7 +145,7 @@ const ServicesSection = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {servicesData[language].map((service, index) => (
+          {servicesData[language as keyof typeof servicesData].map((service, index) => (
             <div
               key={index}
               className="service-card bg-white rounded-lg shadow-md p-6 flex flex-col items-center text-center"
